cd5390c3a1b95b71a2a0bbe37fe5ecd2b5b16e8d
unknown <sheikhalnahian@gmail.com>
2025-11-25 20:48:40 +0600

    product serach by id is added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 
 
@@ -31,6 +31,13 @@ async function run() {
         const query ={}
         const cursor = plantCollection.find(query)
         const result = await cursor.toArray()
+        res.send(result);
+    })
+    app.get('/products/:id',async(req,res)=>{
+        const id = req.params.id;
+        
+        const query = {_id: new ObjectId(id)}
+        const result = await plantCollection.findOne(query)
         res.send(result);
     })
     app.post('/products',async(req,res)=>{
